373cf3feaab98ea9444d841d146b56a83de2cb82
suto-jozsef <66463350+suto-jozsef@users.noreply.github.com>
2024-11-25 13:31:37 +0100

    Update videostream.py
    shifted mypicamera import

--- camera/videostream.py
+++ camera/videostream.py
@@ -1,9 +1,9 @@
 from webcamvideostream import WebcamVideoStream
-from mypicamera import MyPiCamera
 
 class VideoStream:
     def __init__(self, src=0, usePiCamera=False, resolution=(800,600)):
         if usePiCamera:
+            from mypicamera import MyPiCamera
             self.stream = MyPiCamera(resolution=resolution)
         else:
             self.stream = WebcamVideoStream(src=src)
